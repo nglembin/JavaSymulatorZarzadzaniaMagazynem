2b31cc8ab1097f913480d7c640e52ce3a0f4f450
Nikodem Gl. <nikodemglembin@gmail.com>
2025-06-17 17:27:05 +0200

    Update Przyjecie.java

--- src/main/java/pl/glembin/magazyn/model/Przyjecie.java
+++ src/main/java/pl/glembin/magazyn/model/Przyjecie.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 /**
  * Klasa reprezentująca transakcję przyjęcia produktu do magazynu.
  */
+
 public class Przyjecie extends Transakcja {
     public Przyjecie(LocalDate data, String kodProduktu, int ilosc) {
         super(data, kodProduktu, ilosc, TypTransakcji.PRZYJECIE);
